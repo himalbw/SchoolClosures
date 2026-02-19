7e1d33f7b558286505e610bfe0bb7feb52da115c
himalbw <himalb23@gmail.com>
2026-02-19 07:29:17 -0500

    add SchoolYear var for clarity

--- source/clean_outcome_panel.R
+++ source/clean_outcome_panel.R
@@ -2,8 +2,9 @@
 controls_df <- read_csv("./Data/clean/admindist_controls.csv") %>% mutate(sedaadmin = as.numeric(sedaadmin))
 outcomes_df <- read_csv("./Data/clean/admindist_outcomes.csv", show_col_types = FALSE) %>% select(-`...1`) %>% mutate(sedaadmin = as.numeric(sedaadmin), year = as.integer(year))
 panel_df <- outcomes_df %>% left_join(controls_df, by = "sedaadmin")
+panel_df$year <- panel_df$year - 1 # so that we have fall years 
+panel_df <- panel_df %>%mutate(SchoolYear = paste0(year, "-", year + 1))
 closure_df <- read_csv("./Data/clean/panel_closures.csv") %>% rename(sedaadmin = district_id) %>% mutate(sedaadmin = as.numeric(sedaadmin))
-
 panel_joined <- panel_df %>% left_join(closure_df, by = c("sedaadmin", "year")) #93% match rate
 write.csv(panel_joined, "./Data/clean/did_panel.csv")
 
