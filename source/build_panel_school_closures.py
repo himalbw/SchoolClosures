34cd6cedb331fa31afc06b00430562f60126bddf
Julien Berman <julien.d.berman@gmail.com>
2026-02-18 19:42:11 -0500

    new stuff

--- source/build_panel_school_closures.py
+++ source/build_panel_school_closures.py
@@ -6,38 +6,86 @@ from pathlib import Path
 
 def main():
     INDIR = Path("data/raw/closures")
-    INDIR_CW_PERIOD_DATE = Path("data/clean")
-    INDIR_CW_SCHOOL_DISTRICT = Path("data/raw")
+    INDIR_ENROLLMENT = Path("data/raw/controls/School")
+    INDIR_CW = Path("data/raw/crosswalks")
     OUTDIR = Path("data/clean/closures")
     YEARS = list(range(2016, 2024))
 
     df_closures = pd.read_csv(INDIR / "School_Overall_Shares_03.08.23.csv")
-    cw_school_district = pd.read_csv(INDIR_CW_PERIOD_DATE / "cw_school_district.csv")
-    cw_period_date = pd.read_csv(INDIR_CW / "cw_period_date.csv", parse_dates=["date"])
+    df_enrollment_by_school = pd.read_csv(INDIR_ENROLLMENT / "seda_school_pooled.csv")
+    cw_school_district = pd.read_csv(INDIR_CW / "cw_seda.csv")
+
+    df_closures = clean_closures(df_closures)
+    df_closures = add_district(df_closures, cw_school_district)
+    panel = build_panel(df_closures, YEARS)
+
+    panel.to_csv(OUTDIR / "panel_closures.csv", index=False)
 
 def clean_closures(df_closures):
     rename_table = {
         "stateabbrev": "state",
-        "ncesschoolid": "school_id",
+        "ncesschoolid": "nces_school_id",
         "schoolname": "school_name"
     }
     df_closures_clean = (
         df_closures
         .clean_names()
         .rename(columns=rename_table)
-        .assign(state = lambda x: x["state"].str.lower().strip())
-        .assign(school_name = lambda x: x["school_name"].str.lower().strip())
+        .assign(state = lambda x: x["state"].str.lower().str.strip())
+        .assign(school_name = lambda x: x["school_name"].str.lower().str.strip())
         .assign(dosage = lambda x: x["share_virtual"] + x["share_hybrid"])
-        [["state", "match_"]]
+        [["state", "nces_school_id", "school_name", "dosage"]]
     )
     return df_closures_clean
 
 def add_district(df_closures, cw_school_district):
-    pass
+    rename_table = {
+        "ncessch": "nces_school_id",
+        "sedasch": "seda_school_id",
+        "sedaadmin": "district_id"
+    }
+    cw_school_district_clean = (
+        cw_school_district
+        .clean_names()
+        .rename(columns=rename_table)
+        [["nces_school_id", "seda_school_id", "district_id"]]
+    )
+    df_closures_with_district = (
+        df_closures
+        .merge(cw_school_district_clean, how="left", on="nces_school_id")
+        [lambda x: ~x["district_id"].isna()]
+        .drop(columns=["nces_school_id"])
+    )
 
-def build_panel(df_closures, years=[]):
-    pass
+    return df_closures_with_district
+
+
+def aggregate_to_district(df_closures, df_enrollment):
+    rename_table = {
+        "sedasch": "seda_school_id",
+        "": "enrollment"
+    }
+    df_enrollment = (
+        df_enrollment
+        .rename()
+        [["seda_school_id", "enrollment"]]
+    )
+    df_closures_agg = (
+        df_closures
+        .merge(df_enrollment, how="left", on=["seda_school_id"])
+        .assign(weighted_dosage = lambda x: x["dosage"])
+    )
+    return df_closures_agg
+
+
+def build_panel(df_closures, years):
+    ids = list(df_closures["id"].unique())
+    empty_panel = (
+        pd.MultiIndex.from_product([ids, years], names=["id", "year"])
+        .to_frame(index=False)
+    )
+    panel = empty_panel.merge(df_closures, how="left", on=["id"])
+    return panel
 
 if __name__ == "__main__":
     main()
-
